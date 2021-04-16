bdcca9e62706d70ee10b3eee16e543467a96b29b
Evan <ehanz1997@gmail.com>
2021-04-15 22:09:48 -0400

    Finally got around to adding all params and args

--- Player.cpp
+++ Player.cpp
@@ -11,6 +11,26 @@ Player :: Player ()
 
 }
 
+Player :: Player (float ppg, int games, float trbpg, float astpg,
+                  float mpg, float fgp, float tfgp,
+                  float ftp, float efgp, float blkpg, float stlpg,
+                  float per, float ws)
+{
+    setPPG (ppg);
+    setGames (games);
+    setTRBPG (trbpg);
+    setAstPG (astpg);
+    setMPG (mpg);
+    setFGP(fgp);
+    setTFGP (tfgp);
+    setFTP (ftp);
+    setEFGP (efgp);
+    setBLKPG (blkpg);
+    setSTLPG (stlpg);
+    setPER (per);
+    setWS (ws);
+}
+
 //Player Deconstructor
 Player :: ~Player ()
 {
@@ -31,3 +51,146 @@ Player :: getPPG ()
     return p_ppg;
 }
 
+void 
+Player :: setGames (int games)
+{
+    p_games = games;
+}
+
+int
+Player :: getGames ()
+{
+    return p_games;
+}
+
+void
+Player :: setTRBPG (float trbpg)
+{
+    p_trbpg = trb;
+}
+
+float
+Player :: getTRBPG ()
+{
+    return p_trbpg;
+}
+
+void
+Player :: setAstPG (float astpg)
+{
+    p_astpg = astpg;
+}
+
+float
+Player :: getAstPG ()
+{
+    return p_astpg;
+}
+
+void
+Player :: setMPG (float mpg)
+{
+    p_mpg = mpg;
+}
+
+float
+Player :: getMPG ()
+{
+    return mpg;
+}
+
+void
+Player :: setFGP (float fgp)
+{
+    p_fgp = fgp;
+}
+
+float
+Player :: getFGP ()
+{
+    return p_fgp;
+}
+
+void
+Player :: setTFGP (float tfgp)
+{
+    p_tfgp = tfgp;
+}
+
+float
+Player :: getTFGP ()
+{
+    return p_tfgp;
+}
+
+void
+Player :: setFTP (float ftp)
+{
+    p_ftp = ftp;
+}
+
+float
+Player :: getFTP ()
+{
+    return p_ftp;
+}
+
+void
+Player :: setEFGP (float efgp)
+{
+    p_efgp = efgp;
+}
+
+float
+Player :: getEFGP ()
+{
+    return p_efgp;
+}
+
+void
+Player :: setBLKPG (float blkpg)
+{
+    p_blkpg = blkpg;
+}
+
+float
+Player :: getBLKPG ()
+{
+    return p_blkpg;
+}
+
+void
+Player :: setSTLPG (float stlpg)
+{
+    p_stlpg = stlpg;
+}
+
+float
+Player :: getSTLPG ()
+{
+    return p_stlpg;
+}
+
+void
+Player :: setPER (float per)
+{
+    p_per = per;
+}
+
+float
+Player :: getPER ()
+{
+    return p_per;
+}
+
+void
+Player :: setWS (float ws)
+{
+    p_ws = ws;
+}
+
+float
+Player :: getWS ()
+{
+    return p_ws;
+}
--- Player.hpp
+++ Player.hpp
@@ -9,63 +9,81 @@
 class Player 
 {
     public :
+        //Default Constructor and Constructor of Necessary Player Values
         Player ();
+
+        Player (float ppg, int games, float trbpg, float astpg,
+                float mpg, float fgp, float tfgp,
+                float ftp, float efgp, float blkpg, float stlpg,
+                float per, float ws);
         ~Player ();
 
+        //Points Per Game
         void setPPG (float ppg);
         float getPPG ();
 
+        //Games Played
         void setGames (int games);
         int getGames ();
 
-        void setTRB (float trb);
-        float getTRB ();
+        //Total Rebounds a Game
+        void setTRBPG (float trbpg);
+        float getTRBPG ();
 
-        void setAst (float ast);
-        float getAst ();
+        //Assists Per Game
+        void setAstPG (float astpg);
+        float getAstPG ();
 
+        //Minutes Per Game
         void setMPG (float mpg);
         float getMPG ();
 
+        //Field Goal Percentage
         void setFGP (float fgp);
         float getFGP ();
 
+        //3 Point Field Goal Percentage
         void setTFGP (float tfgp);
         float getTFGP ();
 
+        //Free Throw Percentage
         void setFTP (float ftp);
         float getFTP ();
 
+        //Estimated/True Field Goal Percentage
         void setEFGP (float efgp);
-        float getEGFP ();
+        float getEFGP ();
 
-        void setBLK (float blk);
-        float getBLK ();
+        //Blocks Per Game
+        void setBLKPG (float blkpg);
+        float getBLKPG ();
 
-        void setSTL (float stl);
-        float getSTL ();
+        //Steals Per Game
+        void setSTLPG (float stlpg);
+        float getSTLPG ();
 
+        //Player Efficiency Rating
         void setPER (float per);
         float getPER ();
 
+        //Win Shares
         void setWS (float ws);
         float getWS ();
 
     private : 
         float p_ppg;
         int p_games;
-        int p_trb;
-        float p_ast;
+        int p_trbpg;
+        float p_astpg;
         float p_mpg;
         float p_fgp;
         float p_tfgp;
         float p_ftp;
         float p_efgp;
-        float p_blk;
-        float p_stl;
+        float p_blkpg;
+        float p_stlpg;
         float p_per;
         float p_ws;
-
         
 };
 
